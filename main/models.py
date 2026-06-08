7a770cc493112da50d09f520656b1d989cc06a2c
Haiqal <haiqalimanshah2@gmail.com>
2026-06-08 19:17:03 +0800

    add UUID file upload, Argon2 password hashing

--- main/models.py
+++ main/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+import os
+
+def avatar_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join('avatars/', filename)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to=avatar_upload_path, blank=True, null=True)
     bio = models.TextField(blank=True)
 
     def __str__(self):
